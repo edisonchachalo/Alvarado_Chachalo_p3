d3034cc7af807709294ea1cec3f2347c36d66eb5
edisonchachalo <edison.chachalo@udla.edu.ec>
2026-05-26 14:03:14 -0500

    clase ventana

--- src/examenp3p2/Ventana.java
+++ src/examenp3p2/Ventana.java
@@ -1,6 +1,8 @@
 package examenp3p2;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Ventana {
     private JPanel Ventana;
@@ -19,6 +21,50 @@ public class Ventana {
     private JList listP2;
     private JButton btnMostrarP2;
     private JList listP1;
+
+    private InventarioProductos inventario;
+    private DefaultListModel dlmP1;
+    private DefaultListModel dlmP2;
+
+    public Ventana(){
+        inventario = new InventarioProductos();
+        inventario.cargarDatosIniciales();
+
+        dlmP1 = new DefaultListModel();
+        dlmP2 = new DefaultListModel();
+
+
+        btnAgregarP1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    int id = Integer.parseInt(txtIDP1.getText());
+                    String nombre = txtNombreP1.getText();
+                    String categoria = cmbCategoriaP1.getSelectedItem().toString();
+                    int cantidad = Integer.parseInt(txtCantidadP1.getText());
+                    float precio = Float.parseFloat(txtPrecioP1.getText());
+
+                    inventario.agregarProducto(new Producto(id,nombre,categoria,cantidad,precio));
+                    mostrarListaP1();
+                    limpiarCamposP1();
+                } catch (Exception ex){
+                    JOptionPane.showMessageDialog(null, ex.getMessage());
+                }
+            }
+        });
+
+
+        btnModificarP1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int index = listP1.getSelectedIndex();
+                if (index != -1){
+                    Producto seleccionado = inventario.getProductos.get(index);
+                }
+            }
+        });
+    }
+
 
 
 }
